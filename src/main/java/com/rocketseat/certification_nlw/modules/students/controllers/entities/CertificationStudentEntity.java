8751ea39c00dfb80ab7e8dad0519de5085505322
Alexandre Gaspar <alexandretiao16@gmail.com>
2024-02-07 21:38:59 +0100

    feat(entities): Add relationship among entities

--- src/main/java/com/rocketseat/certification_nlw/modules/students/controllers/entities/CertificationStudentEntity.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/controllers/entities/CertificationStudentEntity.java
@@ -3,6 +3,14 @@ package com.rocketseat.certification_nlw.modules.students.controllers.entities;
 import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,12 +18,28 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "certifications")
 public class CertificationStudentEntity {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    private UUID studentID;
+
+    @Column(length = 100)
     private String technology;
+
+    @Column(length = 10)
     private int grate;
+
+    @JoinColumn(name = "student_id")
+    private UUID studentID;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", insertable = false, updatable = false)
+    private StudentEntity studentEntity;
+
+    @OneToMany
+    @JoinColumn(name = "answer_certification_id", insertable = false, updatable = false)
     List<AnswersCertificationsEntity> answersCertificationsEntity;
 
 }
--- src/main/java/com/rocketseat/certification_nlw/modules/students/controllers/entities/StudentEntity.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/controllers/entities/StudentEntity.java
@@ -1,6 +1,15 @@
 package com.rocketseat.certification_nlw.modules.students.controllers.entities;
 
 import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+
 import java.util.List;
 
 import lombok.AllArgsConstructor;
@@ -10,10 +19,17 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "students") // Para o banco de dados reconhecer que isso é uma tabela, ele converte essa classe em uma tabela
 public class StudentEntity {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID) // Gera um código do tipo UUID
     private UUID id;
+
+    @Column(unique = true, nullable = false)
     private String email;
+    
+    @OneToMany(mappedBy = "studentEntity")
     private List<CertificationStudentEntity> CertificationStudentEntity;
 
 }
